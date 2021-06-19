5f69e5654a5197840addc5ac9e0f8039d4f6cf85
Aji Diyantoro <ajidiyantoro@gmail.com>
2021-06-19 23:19:25 +0700

    Modified server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// Call Routes
+const routes = require('./routes');
+routes.app();
+
 app.listen(5000, () => {
     console.log(`Server started on port 5000`);
 });
